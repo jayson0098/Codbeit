2ff9257a6cb77370e73d1938ca085b744e096b36
Jayson R. Alanano <jayzer0098@gmail.com>
2016-06-27 00:27:31 +0900

    First commit to branch 1
    Changes in resources welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Codebe<b>it</b></div>
             </div>
         </div>
     </body>
